90334819667ca4f355cc7f15987b477699b9c9cf
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-06-16 18:42:23 -0400

    modified ECR

--- 50-ECR/provider.tf
+++ 50-ECR/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "expense-infra"
-    key    = "expense-eks" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    key    = "expense-eks-ecr" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "expense-infra"
   }
